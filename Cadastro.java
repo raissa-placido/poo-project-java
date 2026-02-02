b8dc21e5736b9861284773e68ec1675e6895dfd0
Dev-Juliana01 <devjuliana64@gmail.com>
2026-02-02 20:35:17 -0300

    Apagando trecho de código desnessário na classe Cadastro

--- Cadastro.java
+++ Cadastro.java
@@ -63,31 +63,4 @@ public class Cadastro {
         }
         return null;
     }
-
-    // Filtra e mostra apenas os chamados de um usuário específico
-    // public void listarChamadosPorUsuario(Usuario u) {
-    //     System.out.println("\n--- MEUS CHAMADOS (" + u.getNome() + ") ---");
-    //     boolean encontrou = false;
-    //     for (Chamado c : listaChamados) {
-    //         if (c.getSolicitante().getId() == u.getId()) {
-    //             System.out.println(c.toString());
-    //             encontrou = true;
-    //         }
-    //     }
-    //     if (!encontrou) System.out.println("Você não tem chamados abertos.");
-    // }
-
-    // Filtra e mostra apenas os chamados atendidos por este técnico
-    // public void gerarRelatorioTecnico(Tecnico t) {
-    //     System.out.println("\n--- RELATÓRIO DE ATENDIMENTOS (" + t.getNome() + ") ---");
-    //     boolean encontrou = false;
-    //     for (Chamado c : listaChamados) {
-    //         // Verifica se tem técnico E se o ID bate
-    //         if (c.getTecnico() != null && c.getTecnico().getId() == t.getId()) {
-    //             System.out.println(c.toString());
-    //             encontrou = true;
-    //         }
-    //     }
-    //     if (!encontrou) System.out.println("Nenhum atendimento realizado ainda.");
-    // }
 }
